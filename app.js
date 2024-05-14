10e50c043a38f738f99c07224f2e4704cab56916
Anaidys Jimenez <u.anaidys@gmail.com>
2024-05-14 19:41:04 -0400

    workin gon project server

--- app.js
+++ app.js
@@ -18,7 +18,7 @@ const helmet =  require('helmet');
 const dotenv = require('dotenv');
 
 // define routing variables 
-const blogRoute = require('./routes/blogRoute');
+// const blogRoute = require('./routes/blogRoute');
 // const forumRoute = require('./routes/forumRoute')
 // const adminRoute = require('./routes/adminRoute')
 
@@ -47,36 +47,36 @@ app.use(express.static(path.join(__dirname + "/public")));
 // User+Admin GET Paths 
 
 app.get("/", (request, response, next) => {
-    //response.send("This route points to the Home page")
     response.status(200).json({success: {message: "This route points to the Home page"}, statusCode: 200});
 });
 
 
 app.get("/about", (request, response, next) => {
-    //response.send("This route points to the About page")
     response.status(200).json({success: {message: "This route points to the About page"}, statusCode: 200});
 });
 
 app.get("/login", (request, response, next) => {
-    //response.send("This route points to the Login page")
     response.status(200).json({success: {message: "This route points to the Login page"}, statusCode: 200});
 });
 
 // Admin Paths 
 app.get("/admin", (request, response, next) => {
-    //response.send("This route points to the Admin page")
     response.status(200).json({success: {message: "This route points to the Admin page"}, statusCode: 200});
 });
 
 app.get("/admin/create-blog", (request, response, next) => {
     response.status(200).json({success: {message: "This route points to the Create Blog Post page"}, statusCode: 200});
 
+});
+
+// Upload path
+app.post('/upload', (request, response, next) => {
     let file = req.files.image;
     let date = new Date();
     // image name
-    let imagename = date.getDate() + date.getTime() + file.name;
+    let imageName = date.getDate() + date.getTime() + file.name;
     // image upload path
-    let path = 'public/uploads/' + imagename;
+    let path = 'public/uploads/' + imageName;
 
     // create upload
     file.mv(path, (err, result) => {
@@ -84,15 +84,15 @@ app.get("/admin/create-blog", (request, response, next) => {
             throw err;
         } else{
             // our image upload path
-            res.json(`uploads/${imagename}`)
+            res.json(`uploads/${imageName}`)
         }
 })
 });
 
 
+    
 // Route Paths 
-app.use('/', blogRoute);
-app.use('/', forumRoute)
+// app.use('/', blogRoute);
 
 // Server
 app.listen(PORT, () => {
