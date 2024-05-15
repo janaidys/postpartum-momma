19fc96dae29683edaa5709709d604480f16287fe
Anaidys Jimenez <u.anaidys@gmail.com>
2024-05-14 20:57:48 -0400

    developing dummy data

---
+++ data/data.js
@@ -0,0 +1,33 @@
+module.exports = [
+    {
+        banner: "baby-nursery.jpg",
+        title: "Bedtime Routine",
+        article: "A bedtime routine is so essential for both momma and baby. They'll help your newborn adjust their circadian rhythm and associate these activities with night time and sleep. They also help soothe and wined down baby so that they can have optimal sleep. As for momma, this is a great way for you to build in a routine that will help you continue to bond with yur baby and ease some anxiety as you check off boxes. There isn't a one size fits all when it comes to creating schedules and routines, but here is an example of a night time routine that has been working for Anaidys and baby Luca.",
+        upload: "bedtime-routine.png",
+
+    },
+
+    {
+        banner: "diastasis-recti.png",
+        title: "Restore your Core",
+        article: "Have you ever heard of the mom pooch? Well this is pooch is the result of your abs separating to allow your baby to grow with ample space as your uterus expanded. The thing is...now you're not pregnant anymore, but your abs are still separated. Here is an infographic from the Mutu System, who specialize in helping women heal their bodies.",
+        upload: "Diastasis101.jpeg",
+
+    },
+
+    {
+        banner: "",
+        title: "Setting up your Breastfeeding Cart",
+        article: "Let me just start out by saying: Pumping is breastfeeding! For those mommas that who have had difficulties nursing or their lifestyle is better suited for an exclusively pumping journey, more power to you! Setting up a pumping cart will make those late night feedings and overall mobility around your home smoother.",
+        upload: "",
+
+    },
+
+    {
+        banner: "",
+        title: "Choosing the Right Formula for Your Baby",
+        article: "If you're a Costco member, this one is for you! Compared to other leading brands, this Costco generic brand is a much more affordable option. Modeled after breast milk, this formula is packed with vital nutrients for your baby's development without all the artificial growth hormones. Are you a breast feeding momma looking to transition to formula? This may be the formula for you! By Heart prides itself on being the closest to breast milk formula in the market with proven benefits such as easier digestion, less spit-up, more efficient weight gain and enhanced nutrient absorption as compared to a leading infant formula. A colicky baby is a huge NO from me dawg! If you're looking for a formula that will be easy on your baby's tummy, this one is for you! This formula is gentle and helps reduce fusiness, colic, and gas while providing your baby with vital nutrients from milk fat globule membrane (a membrane previously only found in breast milk).",
+        upload: "kirkland-baby-formula.jpeg", ""
+
+    }
+]
