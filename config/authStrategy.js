6389914205e64632c291193e9c37052c24fe9125
Anaidys Jimenez <u.anaidys@gmail.com>
2024-05-20 10:35:25 -0400

    working server

--- config/authStrategy.js
+++ config/authStrategy.js
@@ -37,7 +37,7 @@ passport.use(
   new GithubStrategy({
     clientID:process.env.GITHUB_CLIENT_ID, 
     clientSecret: process.env.GITHUB_CLIENT_SECRET, 
-    callbackURL:''
+    callbackURL:'http://localhost:3000/auth/github'
   },
     (accessToken, refreshToken, profile, done) => {
     console.log(profile);
@@ -51,7 +51,7 @@ passport.use(
     {
       clientID: process.env.GOOGLE_CLIENT_ID,
       clientSecret: process.env.GOOGLE_CLIENT_SECRET,
-      callbackURL: "",
+      callbackURL: "http://localhost:3000/auth/google/",
     },
     (accessToken, refreshToken, profile, done) => {
       console.log(profile);
