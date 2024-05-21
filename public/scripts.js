579407c8d3b0fbcc55d9cd11390ba245885b1272
Anaidys Jimenez <u.anaidys@gmail.com>
2024-05-20 22:59:53 -0400

    patching up the front end

--- public/scripts.js
+++ public/scripts.js
@@ -1,39 +1,5 @@
 // ---CREATE PAGE----
 
-const blogTitleField = document.querySelector(".title");
-const articleField = document.querySelector(".article");
-
-// banner
-const bannerImage = document.querySelector("#banner-upload");
-const banner = document.querySelector(".banner");
-let bannerPath;
-
-const publishBtn = document.querySelector(".publish-btn");
-
-
-bannerImage.addEventListener("change", () => {
-  uploadImage(bannerImage, "banner");
-});
-
-
-// publishing button
-
-publishBtn.addEventListener("click", () => {
-  if (articleField.value.length && blogTitleField.value.length) {
-    // generating id
-    let letters = "abcdefghijklmnopqrstuvwxyz";
-    let blogTitle = blogTitleField.value.split(" ").join("-");
-    let id = "";
-    for (let i = 0; i < 4; i++) {
-      id += letters[Math.floor(Math.random() * letters.length)];
-    }
-
-    // setting up docName
-    let docName = `${blogTitle}-${id}`;
-    let date = new Date(); // for published at info
-
-  }
-});
 
 
 
