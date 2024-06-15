c4de094aeedc2a7f0b42ac1bee616c92d2bdb9c8
Anaidys Jimenez <u.anaidys@gmail.com>
2024-06-15 12:52:52 -0400

    attempting to fix deployment

--- app.js
+++ app.js
@@ -8,7 +8,7 @@ require('./config/authStrategy');
 // packages 
 const express = require('express');
 const morgan = require('morgan');
-const cors = require('cors')
+const cors = require('cors');
 const path = require('path');
 const app = express();
 const PORT = process.env.PORT || 3000; 
